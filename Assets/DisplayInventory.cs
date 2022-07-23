fae9114ed56dc51348a00cc4dd856c16dfdbeb7f
DESKTOP-K6OI6E2\Kai <codekai16@gmail.com>
2022-07-23 13:54:28 -0700

    Minor inventory tweaks

--- Assets/DisplayInventory.cs
+++ Assets/DisplayInventory.cs
@@ -40,10 +40,25 @@ public class DisplayInventory : MonoBehaviour
             obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
             obj.GetComponent<Image>().sprite = inventory.Container[i].food.sprite;
             obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
-		}
+            itemsDisplayed.Add(inventory.Container[i], obj);
+        }
 	}
 
     public Vector2 GetPosition(int i) {
         return new Vector2(xStart + (xSpacing * (i % columns)), yStart + (-ySpacing * (i/columns)));
 	}
+
+    public void UpdateDisplay() {
+        for (int i = 0; i < inventory.Container.Count; i++) {
+            if (itemsDisplayed.ContainsKey(inventory.Container[i])) {
+                itemsDisplayed[inventory.Container[i]].GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
+            } else {
+                var obj = Instantiate(inventory.Container[i].food.prefab, Vector2.zero, Quaternion.identity, transform);
+                obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
+                obj.GetComponent<Image>().sprite = inventory.Container[i].food.sprite;
+                obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
+                itemsDisplayed.Add(inventory.Container[i], obj);
+            }
+        }
+    }
 }
