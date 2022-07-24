f6c515c68b89411c9e5008fa905cbdc70b834262
DESKTOP-K6OI6E2\Kai <codekai16@gmail.com>
2022-07-23 19:56:43 -0700

    Clip to grid

--- Assets/DisplayInventory.cs
+++ Assets/DisplayInventory.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using TMPro;
 using UnityEngine.UI;
 
-public class DisplayInventory : MonoBehaviour
+
+
+public class DisplayInventory : MonoBehaviour, IDropHandler
 {
+
+    public List<SlotSO> slots;
 
     public Inventory inventory;
 
@@ -19,9 +24,34 @@ public class DisplayInventory : MonoBehaviour
 
     public int columns;
     public int rows;
+    private Vector2 currentLoc;
 
     Dictionary<InventorySlot, GameObject> itemsDisplayed = new Dictionary<InventorySlot, GameObject>();
 
+
+    public void OnDrop(PointerEventData eventData) {
+        Debug.Log("Drop");
+        if (eventData.pointerDrag != null) {
+            //GameObject.Find("Inventory/Background")
+            currentLoc = eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition;
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = FindNearestSlot(currentLoc).GetComponent<RectTransform>().anchoredPosition;
+        }
+    }
+
+    //NOTE TO SELF: THINK ABOUT EXTRA CRAFTING SLOTS! LIST APPEND??
+    public GameObject FindNearestSlot(Vector2 loc) {
+        GameObject smallest = slots[0].slot;
+        float smallestD = 10000;
+        for (int i = 0; i < slots.Count; i++) {
+			if (Vector2.Distance(slots[i].slot.GetComponent<RectTransform>().anchoredPosition, loc) < smallestD) {
+                smallest = slots[i].slot;
+                smallestD = Vector2.Distance(slots[i].slot.GetComponent<RectTransform>().anchoredPosition, loc);
+
+            }
+        }
+        return smallest;
+
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -31,10 +61,11 @@ public class DisplayInventory : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //UpdateDisplay();
+        UpdateDisplay();
     }
 
     public void CreateDisplay() {
+
         for (int i = 0; i < inventory.Container.Count; i++) {
             var obj = Instantiate(inventory.Container[i].food.prefab, Vector2.zero, Quaternion.identity, transform);
             obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
@@ -45,7 +76,7 @@ public class DisplayInventory : MonoBehaviour
 	}
 
     public Vector2 GetPosition(int i) {
-        return new Vector2(xStart + (xSpacing * (i % columns)), yStart + (-ySpacing * (i/columns)));
+        return slots[i].slot.GetComponent<RectTransform>().anchoredPosition;
 	}
 
     public void UpdateDisplay() {
---
+++ Assets/dragAndDrop.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class dragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+	private RectTransform rectTransform;
+	private GameObject canvas;
+	private Canvas canvasThing;
+	private CanvasGroup canvasGroup;
+
+	private void Awake() {
+		canvas = GameObject.Find("Canvas");
+		canvasThing = canvas.GetComponent<Canvas>();
+		rectTransform = GetComponent<RectTransform>();
+		canvasGroup = GetComponent<CanvasGroup>();
+	}
+
+	public void OnBeginDrag(PointerEventData eventData) {
+		Debug.Log("Begin Drag");
+		canvasGroup.alpha = .6f;
+		canvasGroup.blocksRaycasts = false;
+	}
+
+	public void OnDrag(PointerEventData eventData) {
+		Debug.Log("Drag");
+		rectTransform.anchoredPosition += eventData.delta / canvasThing.scaleFactor;
+	}
+
+	public void OnEndDrag(PointerEventData eventData) {
+		Debug.Log("End Drag");
+		canvasGroup.alpha = 1f;
+		canvasGroup.blocksRaycasts = true;
+	}
+
+	public void OnPointerDown(PointerEventData eventData) {
+        Debug.Log("Pointer Down");
+	}
+
+
+
+	// Start is called before the first frame update
+	void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
