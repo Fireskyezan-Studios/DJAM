6418927c8abb5a1dc73892c5125b71469fd62906
Fireye <codekai16@gmail.com>
2022-09-21 12:49:30 -0700

    Money pile for testing

--- Assets/Money_Generator.cs
+++ Assets/Money_Generator.cs
@@ -2,10 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Money_Generator : MonoBehaviour
+public class Money_Generator : MonoBehaviour, IInteractable
 {
-    // Start is called before the first frame update
-    void Start()
+	public Inventory inv;
+	public bool canInteract() {
+		return true;
+	}
+
+	public void Interact() {
+		inv.AddMoney(10);
+	}
+
+
+
+	// Start is called before the first frame update
+	void Start()
     {
         
     }
